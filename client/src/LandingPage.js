b3bd1fca22a4aee962c4a4ccfdfc84a3676978e3
JustinMitchell <mitchelljm04@gmail.com>
2023-05-05 14:30:02 -0400

    CSS LandingPage

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -3,6 +3,7 @@ import { Avatar, Button, Paper, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { Box } from '@mui/system'
 import landingBackground from './Images/landingbackground.jpg'
+import StartIcon from '@mui/icons-material/Start';
 
 const LandingPage = ({setLoading}) => {
     const navigate = useNavigate()
@@ -21,8 +22,8 @@ const LandingPage = ({setLoading}) => {
     <Box style={{margin: 100}}> 
     <Avatar sx={{ height: '200px', width: '200px', textAlign: 'center'}} src={'/portfoliophoto.jpg'} />
     <Typography variant='h2' className='landing-hello'>Hello!</Typography>
-    <Typography variant='h4' className='landing-info' >My name is Justin Mitchell, I am a Full-Stack Software Developer specializing in front end developing.</Typography>
-    <Button onClick={handleLoading} className='landing-button'>Continue</Button>
+    <Typography variant='h4' className='landing-info' >My name is Justin Mitchell, I am a Full-Stack Software Developer specializing in Frontend developing specifically React.</Typography>
+    <Button variant='outlined' onClick={handleLoading} endIcon={<StartIcon style={{fontSize: '25px'}}/>} style={{color: 'whitesmoke', fontSize: '25px', marginTop: 20}}>Continue</Button>
     </Box>
     </div>
     </>
